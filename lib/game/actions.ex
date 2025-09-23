ad8780b384bbe853f9076b9e957dc62144592fa9
Vinicius <vinicius.lopes.pimentel@gmail.com>
2025-09-23 15:26:28 -0300

    feat: enhance game mechanics with move handling and attack logic

--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -1,5 +1,6 @@
 defmodule ExMon.Game.Actions do
   alias ExMon.Game
+  alias ExMon.Game.Actions.Attack
 
   def fetch_move(move) do
     Game.player()
@@ -8,8 +9,15 @@ defmodule ExMon.Game.Actions do
   end
 
   defp find_move(moves, move) do
-      Enum.find_value(moves, {:error, moves}, fn {key, value} ->
+      Enum.find_value(moves, {:error, move}, fn {key, value} ->
         if value == move, do: {:ok, key}
       end)
+  end
+
+  def attack(move) do
+    case Game.turn() do
+      :player -> Attack.attack_opponent(:computer, move)
+      :computer -> Attack.attack_opponent(:player, move)
+    end
   end
 end
