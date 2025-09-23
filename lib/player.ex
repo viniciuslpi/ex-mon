cc3177c6ac7ecb0d3300a3641697bb5517ba486f
Vinicius <vinicius.lopes.pimentel@gmail.com>
2025-09-23 11:57:03 -0300

    initialize app

---
+++ lib/player.ex
@@ -0,0 +1,18 @@
+defmodule ExMon.Player do
+  @required_keys [:life, :name, :move_rnd, :move_avg, :move_heal]
+  @max_life 100
+
+  @enforce_keys @required_keys # enforce keys guarantee that the struct is always created with these keys
+  defstruct @required_keys # definition of the struct
+
+  # object criation function
+  def build(name, move_rnd, move_avg, move_heal) do
+    %ExMon.Player{
+      life: @max_life, # module variable
+      name: name,
+      move_rnd: move_rnd,
+      move_avg: move_avg,
+      move_heal: move_heal
+    }
+  end
+end
